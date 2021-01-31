cc09de8881be770ae2aadf657f9a024fb2de65a8
victoriaehrbar <victoriaehrbar@gmail.com>
2021-01-31 16:30:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,19 @@ class Student
     DB[:conn].execute(sql) 
   end
   
-  def save
+   def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
     
   end
 
