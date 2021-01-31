ee14c308dd61f82d9ee15d1a8914f430d358d378
victoriaehrbar <victoriaehrbar@gmail.com>
2021-01-31 16:29:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
     @grade = grade
   end
   
-    def self.create_table
+  def self.create_table
     sql =  <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
